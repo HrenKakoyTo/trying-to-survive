b8b90d6b62ccf415d8957480392d65731874a293
va dim <jus@com.com>
2023-01-21 18:09:21 +0300

    /

--- controller/controller.cpp
+++ controller/controller.cpp
@@ -1,3 +1,54 @@
-//
-// Created by jus on 1/21/23.
-//
+#include "../model/hero.cpp"
+#include "../model/hero.h"
+
+#include "controller.h"
+Controller::Controller()
+{
+    window = new sf::RenderWindow (sf::VideoMode(800, 600), "game");
+}
+
+Controller::~Controller() {
+    //
+}
+
+void Controller::play() {
+    sf::Clock clock;
+    window.setFramerateLimit(60);
+    sf::Event event;
+
+    while (window.pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+            window.close();
+
+       switch (VARIABLE){
+           case 1:
+           {
+               menu_num
+           }
+           case 2:
+           {
+
+           }
+           case 3:
+           {
+
+           }
+       }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
+            hero::move("l");
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
+
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
+
+        }
+
+    }
+
+};
