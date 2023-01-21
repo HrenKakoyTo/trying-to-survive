e6e7a9dc3dc3601861877410201eb1718d53636e
etozheE1m0 <ipopov902@gmail.com>
2023-01-21 15:11:54 +0300

    "controller"

---
+++ visual/start_menu.cpp
@@ -0,0 +1,8 @@
+//
+// Created by spacename on 21.01.2023.
+//
+
+#include "start_menu.h"
+
+
+auto window = get_window();
---
+++ visual/start_menu.h
@@ -0,0 +1,12 @@
+//
+// Created by Алексей on 21.01.2023.
+//
+
+#include "draw_game_elements.h"
+
+class start_menu: public view{
+public:
+    vector <string> hero_icon;
+    vector <string> map;
+    vector <string> description;
+};
