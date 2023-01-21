f4e53781c62d7a0c0c288e7c263745f53e983520
etozheE1m0 <ipopov902@gmail.com>
2023-01-21 15:25:33 +0300

    "controller"

--- visual/draw_game_elements.cpp
+++ visual/draw_game_elements.cpp
@@ -4,14 +4,12 @@
 
 #include "draw_game_elements.h"
 
-void view_game::draw_element(int x, int y, int w, int h, string texture_name, float degree, bool turn) {
+void view_game::draw_element(int x, int y, int w, int h, string texture_name, float degree) {
     auto window = get_window(); // в каждой функции берёшь окно на котором надо рисовать
     Texture game_element;
     game_element.loadFromFile("./texture"+texture_name+".png");
     Sprite s_texture_name;
     s_texture_name.setTexture(game_element);
-    if(turn)
-        s_texture_name.setTextureRect(sf::IntRect(x+w,y,-w,h));
     s_texture_name.setPosition(x, y);
     float g_degree;
     g_degree = degree * (180/3,14);
--- visual/draw_game_elements.h
+++ visual/draw_game_elements.h
@@ -6,6 +6,6 @@
 class view_game: public view
 {
 public:
-    void draw_element(int x, int y, int w, int h, string texture_name, float degree = 0, bool turn = 0);
+    void draw_element(int x, int y, int w, int h, string texture_name, float degree = 0);
     void draw_interface(int hp, int kp);
 };
